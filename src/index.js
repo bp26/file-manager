a70e78a55b47ed46636fb05416e0098c099a190f
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-14 02:24:40 +0300

    feat: add exit line to app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 import { createInterface } from 'readline';
 import { stdin, stdout } from 'process';
-import { getUsername } from './utils/funcs/getUsername.js';
 import Model from './App/Model.js';
 import Controller from './App/Controller.js';
 
 const init = () => {
-  console.log(`Welcome to the File Manager, ${Model.userName}!`);
-  console.log(Model.directory);
+  console.log(`Welcome to the File Manager, ${Model.username}!`);
+  console.log(`You are currently in ${Model.directory}`);
 
   const rl = createInterface(stdin, stdout);
 
   rl.on('line', async (input) => {
+    if (input === '.exit') {
+      process.exit();
+    }
     rl.pause();
     await Controller.runCommand(input);
     rl.resume();
+  });
+
+  process.on('SIGINT', () => {
+    process.exit();
+  });
+
+  process.on('exit', () => {
+    console.log(
+      `Thank you for using File Manager, ${Model.username}, goodbye!`
+    );
   });
 };
 
