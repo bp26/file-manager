895a3ebf0fb66021c2e001640dcf51167a103b88
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-14 02:25:04 +0300

    feat: update transformPath func

--- src/utils/funcs/transformPath.js
+++ src/utils/funcs/transformPath.js
@@ -1,12 +1,5 @@
 import path from 'path';
 
 export const transformPath = (commonPath, dirPath) => {
-  let dir;
-  const arr = dirPath.split('\\');
-  if (arr[0] === '.' || arr[0] === '..') {
-    dir = path.resolve(commonPath, dirPath);
-  } else {
-    dir = dirPath;
-  }
-  return dir;
+  return path.isAbsolute(dirPath) ? dirPath : path.resolve(commonPath, dirPath);
 };
