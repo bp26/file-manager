a80236b9ef16646a4f62f9e78a5cba10e046281a
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-14 00:22:14 +0300

    feat: complete NavigationService

--- src/App/Controller.js
+++ src/App/Controller.js
@@ -1,16 +1,30 @@
+import Model from './Model.js';
 import NavigationService from './Services/NavigationService.js';
 
 class Controller {
   async runCommand(command) {
-    switch (command) {
-      case 'up':
-        break;
-      case 'ls':
-        await NavigationService.listFiles();
-        break;
-      default:
-        console.log('Invalid input');
-        break;
+    const arr = command.split(' ');
+    const baseCommand = arr[0];
+    try {
+      switch (baseCommand) {
+        case 'up':
+          NavigationService.exitDirectory();
+          break;
+        case 'ls':
+          await NavigationService.listFiles();
+          break;
+        case 'cd':
+          await NavigationService.goToDirectory(arr[1]);
+          break;
+        default:
+          console.log('Invalid input');
+          break;
+      }
+    } catch (error) {
+      //console.log('Operation failed');
+      console.log(error);
+    } finally {
+      console.log(Model.directory);
     }
   }
 }
--- src/App/Services/NavigationService.js
+++ src/App/Services/NavigationService.js
@@ -1,14 +1,17 @@
 import Model from '../Model.js';
 import fs from 'fs/promises';
+import path from 'path';
+import { transformPath } from '../../utils/funcs/transformPath.js';
 
 class NavigationService {
   async listFiles() {
-    const files = await fs.readdir(Model.directory, { withFileTypes: true });
+    const files = await fs.readdir(Model.directory);
     const formatedFiles = await Promise.all(
       files.map(async (file) => {
-        const isFile = (await fs.stat(file.path)).isFile();
+        const filepath = path.join(Model.directory, file);
+        const isFile = (await fs.stat(filepath)).isFile();
         return {
-          Name: file.name,
+          Name: file,
           Type: isFile ? 'file' : 'directory',
         };
       })
@@ -16,7 +19,20 @@ class NavigationService {
     console.table(formatedFiles);
   }
 
-  async 
+  exitDirectory() {
+    if (Model.directory.split('\\').length !== 1) {
+      const dir = path.normalize(
+        Model.directory.split('\\').slice(0, -1).join('\\')
+      );
+      Model.directory = dir;
+    }
+  }
+
+  async goToDirectory(path) {
+    const dir = transformPath(Model.directory, path);
+    await fs.readdir(dir);
+    Model.directory = dir;
+  }
 }
 
 export default new NavigationService();
