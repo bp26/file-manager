478f50bceb2fb14fb884b3f12bfd9a4f52f27368
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-15 23:48:56 +0300

    fix: fix getUsername func

--- src/utils/funcs/getUsername.js
+++ src/utils/funcs/getUsername.js
@@ -2,12 +2,13 @@ import { defaultUsername } from '../constants.js';
 
 export const getUsername = () => {
   const args = process.argv.slice(2);
+  console.log(args);
 
-  if (args[0] !== '--') {
+  if (!args[0]) {
     return defaultUsername;
   }
 
-  const splitArg = args[1].split('=');
+  const splitArg = args[0].split('=');
 
   if (splitArg[0] !== '--user') {
     return defaultUsername;
